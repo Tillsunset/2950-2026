914d325b29e5ba21a4625d8b10c4aa3e5d48cac6
Tillsunset <larasebastian001@gmail.com>
2026-03-11 11:42:57 -0500

    fixed imports

--- src/main/java/frc/robot/subsystems/conveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor.java
@@ -1,8 +1,18 @@
 package frc.robot.subsystems;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import frc.robot.subsystems.flywheel;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class conveyor {
 
